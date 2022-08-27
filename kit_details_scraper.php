d49590ce5cabdedf882261a855be5ac5f1977b90
Jeremy <spamalotsmileface@gmail.com>
2022-08-26 17:43:59 -0700

    Update kit_details_scraper.php

--- kit_details_scraper.php
+++ kit_details_scraper.php
@@ -1,10 +1,18 @@
 <?php
 
-$main_menus = ["Main menu theme", "Main Menu Theme", "Main Menu"];
-$round_start;
-$sections = ["Main menu theme", "Bomb planted", "10 seconds remaining on bomb timer", "Choose team", 
-				"Deathcam", "Round Loss", "Round MVP anthem", "10 seconds left in round", "Round start",
-				"Round Won"];
+$main_menu = array("Main menu theme", "Main Menu Theme", "Main Menu", "Main menu theme ", "Main menu theme");
+$bomb_planted = array("Bomb planted", "Bomb planted ", "Bomb Planted ", "Bomb Planted", "Bomb planted", "Bomb planted");
+$bomb_timer = array("10 seconds remaining on bomb timer", "10 seconds remaining on the bomb timer", "Bomb 10 Seconds Countdown", "Bomb 10 Second Countdown");
+$choose_team = array("Choose team", "Choose Team");
+$death_cam = array("Deathcam", "Death Cam");
+$loss = array("Round Loss", "Lost Round", "Round loss");
+$mvp = array("Round MVP anthem", "Round MVP anthem ", "Round MVP Anthem", "MVP Anthem", "MVP Anthem ");
+$left_round = array("10 seconds left in round", "Round 10 Seconds Countdown", "Round 10 Second Countdown");
+$round_start = array("Round start", "Start Round (var. 1)", "Start Round (Variation 1)");
+$round_won = array("Round Won", "Won Round");
+$sections = array($main_menu, $bomb_planted, $bomb_timer, $choose_team, 
+				$death_cam, $loss, $mvp, $left_round, $round_start,
+				$round_won);
 $kitsa = json_decode(file_get_contents("kits.json"), TRUE);
 $kits = $kitsa["kits"];
 $kit_id = 0;
@@ -12,8 +20,11 @@ $json;
 $sections_index = 0;
 $img_sect = 'portable-infobox pi-background';
 
+//$html = file_get_contents($kits['urls'][61]);
+//echo $html;
+
 foreach ($kits['kits'] as $kit) {
-	$section = "Main menu theme";
+	//$section = "Main menu theme";
 	if (strcmp($kits['urls'][$kit_id], "skip") == 0) {
 		$json['details'][$kit]['id'] = 0;
 		$kit_id++;
@@ -22,39 +33,43 @@ foreach ($kits['kits'] as $kit) {
 	$json['details'][$kit]['id'] = $kit_id;
 	$html = file_get_contents($kits['urls'][$kit_id]);
 	if (str_contains($html, $img_sect)) {
+		//echo 'contains yes';
 		$img_a = explode($img_sect, $html);
 		$img_b = explode('<img src="', $img_a[1]);
+		//echo "settings img: " .  explode('" ', $img_b[1])[0];
 		$json['details'][$kit]['img'] = explode('" ', $img_b[1])[0];
 	}
+	
 	foreach ($sections as $section) {
-		if (strcmp($section,"Main menu theme")) {
-			if (str_contains($html, $sections[0])) {
-				$a = explode($section, $html);
-			} else if (str_contains($html, $sections[1])) {
-				$a = explode($sections[1], $html);
-			} else if (str_contains($html, $sections[2])) {
-				$a = explode($sections[2], $html);
+		//echo $html;
+		$set = false;
+		foreach ($section as $sub_section) {
+			if (str_contains($html, $sub_section)) {
+				$a = explode($sub_section, $html);
+				$set = true;
 			}
-		} else {
-			$a = explode($section, $html);
 		}
+		if (!$set) { $json['details'][$kit][array_values($section)[0]] = ""; continue; }
 		$b = explode("</tr>", $a[1]);
+		
+		//$what['what'][$kit][$section[0]] = $b[0];
 		//Method A
 		if (str_contains($b[0], '<td><audio id="')) {
-			$c = explode('<source src="', $b[1]);
-			$json['details'][$kit][$section] = explode('" ', $c[1])[0];
+			$c = explode('<source src="', $b[0]);
+			$json['details'][$kit][array_values($section)[0]] = explode('" ', $c[1])[0];
 			$text = json_encode($json);
 			file_put_contents("out.json", $text, TRUE);
 		}
 		//Method B
 		else
 		{
-			$c = explode('<audio src="', $html);
-			$json['details'][$kit][$section] = explode('" ', $c[1])[0];
+			$c = explode('<audio src="', $b[0]);
+			$json['details'][$kit][array_values($section)[0]] = explode('" ', $c[1])[0];
 			$text = json_encode($json);
 			file_put_contents("out.json", $text, TRUE);
 		}
 	}
 	$kit_id++;
 }
+//file_put_contents("test.txt", json_encode($what), TRUE);
 ?>
